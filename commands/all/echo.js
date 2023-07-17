2706b4deb4cd4ec1049ada80a7c356ea19199de6
iSandRocks <sean.h1313@gmail.com>
2023-07-17 17:12:44 -0400

    echo command added

---
+++ commands/all/echo.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const echo = new SlashCommandBuilder()
+.setName('echo')
+.setDescription('Replies with your input!')
+.addStringOption(option =>
+    option.setName('input')
+        .setDescription('The input to echo back')
+        .setMaxLength(2000));
+
+module.exports = {
+    data: echo,
+    async execute(interaction) {
+        await interaction.reply(`Echo: ${interaction.options.getString('input')}`);
+    }
+};
