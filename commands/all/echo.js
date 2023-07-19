5897fae115fce49fa9a5c889176683bdadc9beee
iSandRocks <sean.h1313@gmail.com>
2023-07-19 13:25:55 -0400

    formatting change

--- commands/all/echo.js
+++ commands/all/echo.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 const echo = new SlashCommandBuilder()
-.setName('echo')
-.setDescription('Replies with your input!')
-.addStringOption(option =>
-    option.setName('input')
-        .setDescription('The input to echo back')
-        .setMaxLength(2000));
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption(option =>
+        option.setName('input')
+            .setDescription('The input to echo back')
+            .setMaxLength(2000));
 
 module.exports = {
     data: echo,
