e5f0539a01a25660fa7e5055a87e31d9c38f1c70
iSandRocks <sean.h1313@gmail.com>
2023-07-20 17:35:15 -0400

    new command

---
+++ commands/fun/embed.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+// inside a command, event listener, etc.
+const exampleEmbed = new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here', inline: true },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+const embedCmd = new SlashCommandBuilder()
+    .setName('embed')
+    .setDescription('sends a embeded message');
+
+    module.exports = {
+        data: embedCmd,
+        async execute(interaction) {
+            interaction.reply({ embeds: [exampleEmbed] });
+        },
+    };
